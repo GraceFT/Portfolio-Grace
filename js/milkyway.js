94b0cb96dc0a626341b9cba125d83b42161200e7
Grace <gracefctorres@gmail.com>
2017-03-31 06:32:23 -0500

    adding finished background and

--- js/milkyway.js
+++ js/milkyway.js
@@ -1,4 +1,236 @@
-//parecido pero no es responsive
+(function () {
+
+    // Star Numbers
+    var starsNumber = 500,
+        canvas = document.getElementById('divCanvas'),
+        context = canvas.getContext('2d'),
+        width = window.innerWidth,
+        height = window.innerHeight,
+        x = 100,
+        y = 100,
+        i = 0,
+        t = 0,
+        stars = [],
+        colors = [
+            '#e7fcff', // Sirius        (Canis Major)
+            '#217cee', // Rigel         (Orion)
+            '#eef310', // Sun & Capella (Auriga)
+            '#fe8028', // Albedaran     (Taurus)
+            '#87f903'  // Betelgeuse    (Orion)
+        ];
+
+    function Star() {
+
+        // Random Position
+        this.x = Math.random() * width;
+        this.y = Math.random() * height;
+
+        // Location Starting
+        this.speed = 0;
+
+        // Colors
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+
+        // Size Limits
+        this.size = Math.random();
+    }
+
+    function draw() {
+
+        // Colors
+        var star;
+
+        // Canvas Size
+        canvas.width = width;
+        canvas.height = height;
+
+        for (t = 0; t < stars.length; t += 1) {
+
+            // Getting Star
+            star = stars[t];
+            
+            // Building
+            context.beginPath();
+            context.fillStyle = star.color;
+            context.arc(star.x, star.y, star.size, Math.PI * 2, false);
+            context.fill();
+
+            // Animation
+            star.x -= star.speed;
+
+            // Keeping Stars on canvas
+            if (star.x < -star.size) {
+                star.x = width + star.size;
+            }
+
+            if (star.size < 5) {
+                star.speed = 1;
+            }
+
+            if (star.size < 4) {
+                star.speed = 0.5;
+            }
+
+            if (star.size < 3) {
+                star.speed = 0.25;
+            }
+        }
+    }
+
+    for (i = 0; i < starsNumber; i += 1) {
+        stars.push(new Star());
+    }
+
+    setInterval(draw, 20);
+
+}()); 
+
+/*(function() {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || 
+								  window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+	  window.requestAnimationFrame = requestAnimationFrame;
+	})();
+
+	// Terrain stuff.
+	var terrain = document.getElementById("terCanvas"),
+		background = document.getElementById("bgCanvas"),
+		//terCtx = terrain.getContext("2d"),
+		bgCtx = background.getContext("2d"),
+		width = window.innerWidth,
+		height = document.body.offsetHeight;
+    //(height < 400)?height = 3000:height;
+
+	//terrain.width = background.width = width;
+	//terrain.height = background.height = height;
+
+	// Some random points
+	var points = [],
+		displacement = 140,
+		power = Math.pow(2,Math.ceil(Math.log(width)/(Math.log(2))));
+	
+	// set the start height and end height for the terrain
+	points[0] = (height - (Math.random()*height/2))-displacement;
+	points[power] = (height - (Math.random()*height/2))-displacement;
+
+	// create the rest of the points
+	for(var i = 1; i<power; i*=2){
+		for(var j = (power/i)/2; j <power; j+=power/i){
+			points[j] = ((points[j - (power/i)/2] + points[j + (power/i)/2]) / 2) + Math.floor(Math.random()*-displacement+displacement );
+		}
+		displacement *= 0.6;
+	}
+
+	// draw the terrain
+	/*terCtx.beginPath();
+					
+	for(var i = 0; i<=width; i++){
+		if(i === 0){
+			terCtx.moveTo(0, points[0]);
+		}else if(points[i] !== undefined){
+			terCtx.lineTo(i, points[i]);
+		}
+	}
+
+	terCtx.lineTo(width,terrain.height);
+	terCtx.lineTo(0,terrain.height);
+	terCtx.lineTo(0,points[0]);
+	terCtx.fill();*/
+
+/*
+	// Second canvas used for the stars
+	bgCtx.fillStyle = '#05004c';
+	bgCtx.fillRect(0,0,width,height);
+
+	// stars
+	function Star(options){
+		this.size = Math.random()*2;
+		this.speed = Math.random()*.1;
+		this.x = options.x;
+		this.y = options.y;
+	}
+
+	Star.prototype.reset = function(){
+		this.size = Math.random()*2;
+		this.speed = Math.random()*.1;
+		this.x = width;
+		this.y = Math.random()*height;
+	}
+	
+	Star.prototype.update = function(){
+		this.x-=this.speed;
+		if(this.x<0){
+		  this.reset();
+		}else{
+		  bgCtx.fillRect(this.x,this.y,this.size,this.size); 
+		}
+	}
+	
+	function ShootingStar(){
+		this.reset();
+	}
+	
+	ShootingStar.prototype.reset = function(){
+		this.x = Math.random()*width;
+		this.y = 0;
+		this.len = (Math.random()*80)+10;
+		this.speed = (Math.random()*10)+6;
+		this.size = (Math.random()*1)+0.1;
+    // this is used so the shooting stars arent constant
+		this.waitTime =  new Date().getTime() + (Math.random()*3000)+500;
+		this.active = false;
+	}
+	
+	ShootingStar.prototype.update = function(){
+		if(this.active){
+			this.x-=this.speed;
+			this.y+=this.speed;
+			if(this.x<0 || this.y >= height){
+			  this.reset();
+			}else{
+			bgCtx.lineWidth = this.size;
+				bgCtx.beginPath();
+				bgCtx.moveTo(this.x,this.y);
+				bgCtx.lineTo(this.x+this.len, this.y-this.len);
+				bgCtx.stroke();
+			}
+		}else{
+			if(this.waitTime < new Date().getTime()){
+				this.active = true;
+			}			
+		}
+	}
+
+	var entities = [];
+	
+	// init the stars
+	for(var i=0; i < height; i++){
+		entities.push(new Star({x:Math.random()*width, y:Math.random()*height}));
+	}
+  
+	// Add 2 shooting stars that just cycle.
+	entities.push(new ShootingStar());
+	entities.push(new ShootingStar());
+	
+	//animate background
+	function animate(){
+		bgCtx.fillStyle = '#05004c';
+		bgCtx.fillRect(0,0,width,height);
+		bgCtx.fillStyle = '#ffffff';
+		bgCtx.strokeStyle = '#ffffff';
+
+		var entLen = entities.length;
+	  
+		while(entLen--){
+			entities[entLen].update();
+		}
+		
+		requestAnimationFrame(animate);
+	}
+	animate();*
+*/
+/*//parecido pero no es responsive
 var $freezed = false;
 var $speddOffset = 0;
 var $canvas=document.getElementById('staryNight');
@@ -81,7 +313,7 @@ function drawStars() {
 }
 populateSky();
 setInterval(drawStars,1);
-
+*/
 
 
 
