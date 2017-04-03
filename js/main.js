97362666a8a7569745dd5a1ab09ecf2db266f524
Grace <gracefctorres@gmail.com>
2017-04-03 05:02:04 -0500

    adding animation background

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ function init()
     $('#btn_project').click(onClickProjects);
     $('#btn_contact_me').click(onClickContactMe);
     
+    
 }
 ///+++++++++++++++++++++++++++++++++++++++PAGINATION++++++++++++++++++++++++++++++++++++///
 function onClickInitial()
