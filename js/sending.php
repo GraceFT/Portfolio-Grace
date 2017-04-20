96c0f1d09d9501b3c2ac5e90c2b55930e5bdd9dd
Grace <gracefctorres@gmail.com>
2017-04-20 12:54:57 -0500

    quit form

--- js/sending.php
+++
@@ -1,9 +0,0 @@
-<?php
-$destino= "gracefctorres@gmail.com";
-$nombre = $_POST["nombre"];
-$email = $_POST["email"];
-$mensaje = $_POST["mensaje"];
-$contenido= "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
-mail($destino, "Contacto", $contenido);
-?>
-    
